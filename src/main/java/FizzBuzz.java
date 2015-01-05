8e82266bc7f6d7bb83690fe91010f6b5cec8b4fa
mdurban <murban117@gmail.com>
2015-01-05 12:25:31 -0500

    solution that uses no conditional statements

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,17 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class FizzBuzz {
+	
+		
+	private static Map<Integer, String> map1 = new HashMap<>();
+	private static Map<Integer, String> map2 = new HashMap<>();
+	private static Map<Integer, String> map3 = new HashMap<>();
 
-	public static String print(int i) {
-		if ((isDivisibleByThree(i) && isDivisibleByFive(i)) || (containsThree(i) && containsFive(i))) {
-			return "FizzBuzz";
-		} else if (isDivisibleByThree(i) || containsThree(i)) {
-			return "Fizz";
-		} else if (isDivisibleByFive(i) || containsFive(i)) {
-			return "Buzz";
-		} else {
-			return Integer.toString(i);
-		}
+	static {
+		map1.put(0, "Fizz");
+		map2.put(0, "Buzz");
+		map3.put(0, "FizzBuzz");
 	}
+
+	public static String print(int intToPrint) {
+			StringBuilder sb = new StringBuilder();
+			int modThree = intToPrint % 3;
+			int modFive = intToPrint % 5;
+
+			sb.append(map1.getOrDefault(modThree, map2.getOrDefault(modFive, Integer.toString(intToPrint))));
+			sb = new StringBuilder(map3.getOrDefault(modThree + modFive, sb.toString()));
+
+			return sb.toString();
+	}
+
+
+//	public static String print(int i) {
+//		if ((isDivisibleByThree(i) && isDivisibleByFive(i)) || (containsThree(i) && containsFive(i))) {
+//			return "FizzBuzz";
+//		} else if (isDivisibleByThree(i) || containsThree(i)) {
+//			return "Fizz";
+//		} else if (isDivisibleByFive(i) || containsFive(i)) {
+//			return "Buzz";
+//		} else {
+//			return Integer.toString(i);
+//		}
+//	}
 
 	private static boolean isDivisibleByFive(int i) {
 		return i % 5 == 0;
